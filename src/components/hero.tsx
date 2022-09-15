a402be94c842d94dd4449dda1d039f8f8810468d
Lucho <lucho14garfield@gmail.com>
2022-09-16 00:09:14 +0100

    feat: Finishing hero. Adding spline package..

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,9 +1,10 @@
 import { FunctionComponent } from 'react'
 import logo from 'public/dwyl_logo.png'
+import hero_img from 'public/hero.png'
 
-const Header: FunctionComponent = () => {
+const Hero: FunctionComponent = () => {
   return (
-    <>
+    <div className='min-h-screen max-h-screen w-screen flex flex-col'>
       <div className="h-fit flex flex-row flex-nowrap justify-between items-center pl-10 pr-10 pt-10">
         <div className="w-24 h-12">
           <img className="max-h-full max-w-full" src={logo} />
@@ -20,8 +21,24 @@ const Header: FunctionComponent = () => {
           </button>
         </div>
       </div>
-    </>
+
+      <img className="h-auto w-9/12 ml-auto mr-auto mt-12" src={hero_img} />
+
+      <div className="flex flex-1 justify-center flex-col">
+        <div className="text-center pr-5 pl-5">
+          <h2 className="font-semibold text-4xl tracking-tight leading-none">
+            <p className="text-green-900">software</p>
+            <p className="text-green-900/50">made simple.</p>
+          </h2>
+
+          <div className='mt-5 text-base'>
+            <p className='text-gray-600 leading-5'>we offer software development services and training.</p>
+            <p className='font-thin text-gray-600 mt-2'>based in London.</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default Header
+export default Hero
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import Header from 'components/header'
+import Hero from 'components/hero'
 import { createRoot } from 'react-dom/client'
 import 'tailwindcss/tailwind.css'
 
@@ -7,27 +7,8 @@ const root = createRoot(container)
 
 function App() {
   return (
-    <div className="font-montserrat bg-white min-h-screen flex flex-col">
-      <div className='h-screen w-screen'>
-        <Header />
-
-        <div className="h-full mx-auto max-w-screen-xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-          <div className="text-center">
-            <h2 className="text-base font-semibold uppercase tracking-wide text-blue-600">
-              Welcome to
-            </h2>
-            <p className="my-3 text-4xl font-bold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
-              reactjs-vite-tailwindcss-boilerplate
-            </p>
-            <p className="text-xl text-gray-400">Start building for free.</p>
-          </div>
-        </div>
-      </div>
-
-      <div className='h-20 bg-yellow-50 w-full'>
-
-      </div>
-
+    <div className="font-montserrat bg-white">
+      <Hero />
     </div>
   )
 }
