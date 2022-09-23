832d64b338d8bf8cb2c44f0d84e38b2981246580
Lucho <lucho14garfield@gmail.com>
2022-09-24 00:17:40 +0100

    feat: Hero with spline on desktop devices.

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,13 +2,26 @@ import { FunctionComponent, useEffect, useRef } from 'react'
 import logo from 'public/dwyl_logo.png'
 import hero_img from 'public/hero.png'
 import gsap from 'gsap'
-import { staggerReveal, fadeUpReveal } from 'utils'
+import { staggerReveal, fadeUpReveal, useMediaQuery } from 'utils'
+import Spline from '@splinetool/react-spline'
+
+const renderImage = (isDesktop: boolean) => {
+  return isDesktop ? (
+    <div className="absolute h-full w-full">
+      <Spline scene="https://prod.spline.design/MWszCFbtlIdlZM4d/scene.splinecode" onStart={() => console.log("started")} onLoad={() => console.log("load")} />
+    </div>
+  ) : (
+    <img className="ml-auto mr-auto mt-12 h-auto w-[35vh]" src={hero_img} />
+  )
+}
 
 const Hero: FunctionComponent = () => {
   const titleRef = useRef(null)
   const subtitleRef = useRef(null)
   const description1Ref = useRef(null)
   const description2Ref = useRef(null)
+
+  const isDesktop = useMediaQuery('(min-width: 1024px)')
 
   useEffect(() => {
     const titleEl = titleRef.current
@@ -25,7 +38,7 @@ const Hero: FunctionComponent = () => {
 
   return (
     <div className="flex max-h-screen min-h-screen w-screen flex-col">
-      <div className="flex h-fit flex-row flex-nowrap items-center justify-between pl-10 pr-10 pt-10">
+      <div className="z-10 flex h-fit flex-row flex-nowrap items-center justify-between pl-10 pr-10 pt-10">
         <div className="h-12 w-24">
           <img className="max-h-full max-w-full" src={logo} />
         </div>
@@ -35,18 +48,18 @@ const Hero: FunctionComponent = () => {
             className="inline-block h-10 max-h-full w-16 max-w-full cursor-pointer rounded-full bg-green-700 px-2.5 
                 py-2.5 text-xs font-semibold leading-tight text-white
                 shadow-md transition duration-150 ease-in-out hover:bg-green-800 hover:shadow-lg 
-                focus:bg-green-800 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-900 active:shadow-lg"
+                focus:bg-green-800 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-900 active:shadow-lg "
           >
             app
           </button>
         </div>
       </div>
 
-      <img className="ml-auto mr-auto mt-12 h-auto w-[35vh]" src={hero_img} />
+      {renderImage(isDesktop)}
 
-      <div className="flex flex-1 flex-col justify-center">
-        <div className="pr-5 pl-5 text-center">
-          <div className="relative flex h-[2.5rem] w-full flex-row justify-center overflow-hidden md:h-[3.5rem]">
+      <div className="z-10 flex flex-1 flex-col justify-end lg:flex-row lg:justify-start">
+        <div className="mb-[5%] pr-5 pl-5 text-center lg:mb-0 lg:flex lg:w-1/2 lg:flex-col lg:justify-center lg:pr-0 lg:pl-[10%]">
+          <div className="relative flex h-[2.5rem] w-full flex-row justify-center overflow-hidden md:h-[3.5rem] lg:justify-start">
             <p
               className="absolute text-4xl font-semibold leading-none tracking-tight text-green-900 md:text-[3.5rem]"
               ref={titleRef}
@@ -54,7 +67,7 @@ const Hero: FunctionComponent = () => {
               software
             </p>
           </div>
-          <div className="relative flex h-[2.5rem] w-full flex-row justify-center overflow-hidden md:h-[3.5rem]">
+          <div className="relative flex h-[2.5rem] w-full flex-row justify-center overflow-hidden md:h-[3.5rem] lg:justify-start">
             <p
               className="absolute text-4xl font-semibold leading-none tracking-tight text-green-900/50 md:text-[3.5rem]"
               ref={subtitleRef}
@@ -64,15 +77,15 @@ const Hero: FunctionComponent = () => {
           </div>
 
           <div className="mt-5 text-base md:text-lg">
-            <div className="relative flex h-[2.5rem] w-full flex-row justify-center overflow-hidden md:h-[18px]">
+            <div className="relative flex h-[2.5rem] w-full flex-row justify-center overflow-hidden md:h-[18px] lg:h-[45px] lg:justify-start">
               <p
-                className="absolute h-[1.5rem] leading-5 text-gray-600"
+                className="absolute h-[1.5rem] leading-5 text-gray-600 lg:text-start"
                 ref={description1Ref}
               >
                 we offer software development services and training.
               </p>
             </div>
-            <div className="relative flex h-[2.5rem] w-full flex-row justify-center overflow-hidden md:h-[2.5rem]">
+            <div className="relative flex h-[2.5rem] w-full flex-row justify-center overflow-hidden md:h-[2.5rem] lg:justify-start">
               <p
                 className="absolute mt-2 h-[1.5rem] font-thin text-gray-600"
                 ref={description2Ref}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 export { staggerReveal, fadeUpReveal } from "./anims"
+export { useMediaQuery}  from './hooks'
 
 export function classNames(...classes: unknown[]): string {
   return classes.filter(Boolean).join(' ')
