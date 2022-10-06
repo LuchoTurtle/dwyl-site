e9462bf0440beab611b904ca9223804dc59a6c7e
Lucho <lucho14garfield@gmail.com>
2022-10-06 15:11:52 +0100

    fix: Fixing dev serving time.

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,26 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import ViteFaviconsPlugin from 'vite-plugin-favicon'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths(), ViteFaviconsPlugin('./src/assets/favicon.png')],
-  assetsInclude: ['**/*.spline']
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  if (command === 'serve') {
+    // dev specific config
+    return {
+      plugins: [
+        react(), 
+        tsconfigPaths(), 
+        //ViteFaviconsPlugin('./src/assets/favicon.png')
+      ],
+      assetsInclude: ['**/*.spline']
+    }
+  } else {
+    // build specific config
+    return {
+      plugins: [
+        react(), 
+        tsconfigPaths(), 
+        ViteFaviconsPlugin('./src/assets/favicon.png')
+      ],
+      assetsInclude: ['**/*.spline']
+    }
+  }
 })
-
