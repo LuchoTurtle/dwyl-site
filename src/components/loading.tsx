f900d8f202c34de0b3cf14a033f8a48c207972f6
Lucho <lucho14garfield@gmail.com>
2022-10-07 01:46:03 +0100

    fix: Converting Loading to functional component

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,53 +1,44 @@
-import React, { Component } from 'react'
+import React, { Component, FunctionComponent, useEffect, useState } from 'react'
 import gsap from 'gsap'
 
-class Loading extends Component {
-  state = {
-    progress: 0
-  }
-  intervalId: any
+const Loading: FunctionComponent = () => {
+  
+  const [progress, setProgress] = useState(0)
 
-  componentDidMount() {
-    this.intervalId = setInterval(() => {
-      this.setState({
-        progress: this.state.progress + (1 - this.state.progress) * 0.1
-      })
+  useEffect(() => {
+
+    const intervalId = setInterval(() => {
+      setProgress(progress + (1 - progress) * 0.1)
     }, 500 + 200 * Math.random())
-  }
 
-  componentDidUpdate(prevProps: any) {
-    //console.log(this.state.progress)
-  }
+    return () => {
+      clearInterval(intervalId)
+    }
+  }, [progress])
 
-  componentWillUnmount() {
-    clearInterval(this.intervalId)
-  }
-
-  render() {
     return (
       <div className='h-full w-full z-50 fixed'>
         <div className='absolute h-full w-full bg-white'></div>
         <div
-          style={{ opacity: 1 - this.state.progress }}
+          style={{ opacity: 1 - progress }}
           className="absolute  h-full  w-full bg-neutral-800 transition-[opacity] duration-700 ease-out"
         />
         <div className="absolute flex h-full w-full flex-col items-center justify-center  font-montserrat ">
           <h1
-            style={{ opacity: this.state.progress }}
+            style={{ opacity: progress }}
             className="absolute text-7xl font-extralight text-black"
           >
-            {Math.floor(this.state.progress * 100) + '%'}
+            {Math.floor(progress * 100) + '%'}
           </h1>
           <h1
-            style={{ opacity: 1 - this.state.progress }}
+            style={{ opacity: 1 - progress }}
             className="absolute text-7xl font-extralight text-white"
           >
-            {Math.floor(this.state.progress * 100) + '%'}
+            {Math.floor(progress * 100) + '%'}
           </h1>
         </div>
       </div>
     )
-  }
 }
 
 export default Loading
